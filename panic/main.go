ec7b4df3da7484191a2570989c2222ef4258decc
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-03 20:49:49 +0300

    add panic

---
+++ panic/main.go
@@ -0,0 +1,19 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+func main() {
+	// messages := []string{
+	// 	"massage 1",
+	// 	"massage 2",
+	// 	"massage 3",
+	// 	"massage 4",
+	// }
+
+	// messages[4] = "messages 5"
+	// fmt.Println(messages)
+
+	panic("aaaaaaaa help")
+}
