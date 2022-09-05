2e18a726bcec2c889d4fd04866fa1382bf8c429b
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-05 16:39:52 +0300

    add multiply assignment and swap

--- variables/main.go
+++ variables/main.go
@@ -17,4 +17,13 @@ func main() {
 	fmt.Println(reflect.TypeOf(message))
 	fmt.Println(reflect.TypeOf(firstName))
 	fmt.Println(reflect.TypeOf(birthadayYear))
+
+	var c, d int // Multiple assignment
+	c, d = 3, 4  // The value is assigned in order: a == 5 и b == 10
+
+	var a, b int
+	a, b = 2, 1 // a == 5, b == 10
+	a, b = b, a // swap: a == 10, b == 5
+
+	fmt.Println(a, b, c, d)
 }
