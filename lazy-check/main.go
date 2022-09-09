e38589908d48920964beae59b2b8150bb324a689
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-09 09:10:02 +0300

    add lazy check

---
+++ lazy-check/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a, b := 1, 0
+
+	incB := func() bool {
+		b = b + 1
+		return true
+	}
+
+	if a == 1 || incB() {
+		fmt.Println("Hello")
+	}
+
+	fmt.Println(a, b)
+}
