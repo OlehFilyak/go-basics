acce4f640de91d56e607da04f95d03edceb46a21
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-09 08:55:42 +0300

    add one example

--- scope/main.go
+++ scope/main.go
@@ -2,9 +2,24 @@ package main
 
 import "fmt"
 
-var a, b, c int // доступні в області бачення поточного пакету
+var a, b, c int // variables are available in the viewport of the current package
 
 func main() {
+	i := 10
+	if i == 10 {
+		// change the value of the variable i
+		i += 5
+		if i == 15 {
+			// this block creates a new variable i, which
+			// overrides the variable with the same name defined above
+			// such situations should be avoided in practice
+			i := 7
+			fmt.Println(i)
+			// the scope of this variable is limited to the block
+		}
+	}
+	fmt.Println(i)
+
 	a, b, c := 1, 2, 3 // local variables
 	fmt.Println(a, b, c)
 
