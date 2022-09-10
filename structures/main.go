438ddf22dcefdea815f37d9663da9de39175879e
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-10 18:58:36 +0300

    some changes

--- structures/main.go
+++ structures/main.go
@@ -4,88 +4,25 @@ import (
 	"fmt"
 )
 
-type Age int
-
-func (age Age) isAdult() bool {
-	return age >= 18
-}
-
-type User struct {
-	name   string
-	age    Age
-	sex    string
-	weight int
-	height int
-}
-
-func NewUser(name, sex string, age, weight, height int) User {
-	return User{
-		name:   name,
-		sex:    sex,
-		age:    Age(age),
-		weight: weight,
-		height: height,
-	}
-}
-
-func (user *User) setName(name string) {
-	user.name = name
-}
-
-func (user User) getName() string {
-	return user.name
-}
-
-// func (user User) printUserInfo() { //value
-// 	fmt.Println(user.name, user.age, user.sex, user.height)
-// }
-
-// func (user *User) printUserInfo() { //pointer
-// 	fmt.Println(user.name, user.age, user.sex, user.height)
-// }
-
-// type DumbDatabase struct {
-// 	m map[string]string
-// }
-
-// func NewDumbDatabase() *DumbDatabase {
-// 	return &DumbDatabase{
-// 		m: make(map[string]string),
-// 	}
-// }
-
 func main() {
-	// user := struct {
-	// 	name   string
-	// 	age    int
-	// 	sex    string
-	// 	weight int
-	// 	height int
-	// }{"Boryviter", 55, "Male", 75, 177}
+	// INITIALIZATION 1
+	// p := Person{}
+	// or
+	// var p Person
 
-	// fmt.Println(user)
-	// fmt.Printf("%+v\n", user)
+	// INITIALIZATION 2
+	// date := time.Date(2000, 12, 1, 0, 0, 0, 0, time.UTC)
+	// p := Person{ "Иван", "ivan@yandex.ru", date }
 
-	user1 := NewUser("Ivan", "Male", 23, 77, 199)
-	user2 := User{"Petro", 25, "Male", 79, 184}
+	// INITIALIZATION 3
+	// p := Person{Name: "Иван", Email: "ivan@yandex.ru"}
 
-	// fmt.Println(user1.weight)
-	// fmt.Printf("%+v\n", user1)
-	// fmt.Printf("%+v\n", user2)
-
-	// printUserInfo(user1)
-	// printUserInfo(user2)
-
-	// user1.printUserInfo()
-	// user2.printUserInfo()
-
-	fmt.Println(user1.age.isAdult())
-	fmt.Println(user1.getName())
-	fmt.Println(user2.getName())
-	user2.setName("Ioan")
-	fmt.Println(user2.getName())
+	// func NewPerson(name, email string, dobYear, dobMonth, dobDate int) Person {
+	// 	return Person{
+	// 		Name:        name,
+	// 		Email:       email,
+	// 		dateOfBirth: time.Date(dobYear, time.Month(dobMonth), dobDate, 0, 0, 0, 0, time.UTC),
+	// }
+	// }
+	fmt.Println()
 }
-
-// func printUserInfo(user User) {
-// 	fmt.Println(user.name, user.age, user.sex, user.height)
-// }
