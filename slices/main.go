1bb12cf54808500e278baf5d17aea44cae80af30
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-19 07:48:45 +0300

    Update main.go

--- slices/main.go
+++ slices/main.go
@@ -1,50 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
 
-	messagesSlice := []string{"4", "5", "6"}
-	var emptySlice []string
-	emptySlice5Len := make([]string, 5)
+	var a []int
+	var b []int = []int{1, 2, 3}
+	c := []int{1, 2, 3}
+	d := []int{1: 12}
 
-	fmt.Println(emptySlice5Len)
-	emptySlice5Len[4] = "44444"
-	fmt.Println(emptySlice5Len)
-	emptySlice5Len = append(emptySlice5Len, "321")
-	fmt.Println(emptySlice5Len)
-	fmt.Println(len(emptySlice5Len))
-	fmt.Println(cap(emptySlice5Len))
-	// emptySlice[0] = "2" // error
-	fmt.Println(emptySlice)
-	fmt.Println(len(emptySlice))
-	// fmt.Println(cap(emptySlice))
+	fmt.Println(a) // []
+	fmt.Println(b) // [1 2 3]
+	fmt.Println(c) // [1 2 3]
+	fmt.Println(d) // [0 12]
 
-	fmt.Println(messagesSlice)
-
-	printMessagesSlices(messagesSlice)
-	fmt.Println(messagesSlice)
-
-	// weekTempArr := [7]int{1, 2, 3, 4, 5, 6, 7}
-	// workDaysSlice := weekTempArr[:5]
-	// weekendSlice := weekTempArr[5:]
-	// fromTuesdayToThursDaySlice := weekTempArr[1:4]
-	// weekTempSlice := weekTempArr[:]
-
-	// fmt.Println(workDaysSlice, len(workDaysSlice), cap(workDaysSlice))                                        // [1 2 3 4 5] 5 7
-	// fmt.Println(weekendSlice, len(weekendSlice), cap(weekendSlice))                                           // [6 7] 2 2
-	// fmt.Println(fromTuesdayToThursDaySlice, len(fromTuesdayToThursDaySlice), cap(fromTuesdayToThursDaySlice)) // [2 3 4] 3 6
-	// fmt.Println(weekTempSlice, len(weekTempSlice), cap(weekTempSlice))                                        // [1 2 3 4 5 6 7] 7 7
-}
-
-func printMessagesSlices(messagesSlice []string) error {
-	if len(messagesSlice) == 0 {
-		return errors.New("empty array")
-	}
-	messagesSlice[1] = "111111111"
-	fmt.Println(messagesSlice)
-	return nil
+	x := make([]int, 10, 10) // [0 0 0 0 0 0 0 0 0 0]
+	fmt.Println(x)
 }
