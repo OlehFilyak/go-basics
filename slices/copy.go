7d16fba7f298dbaa05cf2399d4f4ce7638b51ec1
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-24 07:21:20 +0300

    Create copy.go

---
+++ slices/copy.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := []int{1, 2, 3}
+	b := make([]int, 3, 3)
+	n := copy(b, a)
+
+	fmt.Printf("a = %v\n", a)                  // a = [1 2 3]
+	fmt.Printf("b = %v\n", b)                  // b = [1 2 3]
+	fmt.Printf("Скопировано %d элемента\n", n) // Скопировано 3 элемента
+}
