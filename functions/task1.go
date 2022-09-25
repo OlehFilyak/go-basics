ad10c36b95ce3b75e16ac8f36686f7d7a2d0ba10
Олег Філяк <oleh.filyak92@gmail.com>
2022-09-25 18:51:04 +0300

    Create task1.go

---
+++ functions/task1.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// Напишите функцию, находящую наименьшее из четырех введённых в этой же функции чисел.
+// Входные данные
+// Вводится четыре числа.
+// Выходные данные
+// Необходимо вернуть из функции наименьшее из 4-х данных чисел
+// Sample Input:
+// 4 5 6 7
+// Sample Output:
+// 4
+
+func minimumFromFour() int {
+	//print your code here
+	var a, b, c, d int
+	fmt.Scan(&a, &b, &c, &d)
+	var min int
+	if a <= b && a <= c && a <= d {
+		return a
+	}
+	if b <= a && b <= c && b <= d {
+		return b
+	}
+	if c <= a && c <= b && c <= d {
+		return c
+	}
+	if d <= a && d <= c && d <= b {
+		return d
+	}
+
+	return min
+}
